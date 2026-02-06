fe904f375ed1f2b75907092ebf6dc4b4e8c61b35
Yash Ghorpade <yashghorpade2002@gmail.com>
2026-02-06 13:05:10 +0530

    debug step

--- jenkinsfile_k8s.groovy
+++ jenkinsfile_k8s.groovy
@@ -4,6 +4,7 @@ pipeline{
         stage('start minikube') {
             steps {
                 sh 'echo "checking the minikube status and starting it"'
+                sh 'whoami'
                 sh 'minikube start -- device = docker'
             }
         }
